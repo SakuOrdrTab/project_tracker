a1f0cb9c69e3d5472b58a3879aa7086134bb16af
SakuOrdrTab <leinosensaku@gmail.com>
2024-02-19 20:28:51 +0200

    wrapped the program functionality to a class

--- proj_ttrack.py
+++ proj_ttrack.py
@@ -4,66 +4,69 @@ import sys
 import pandas as pd
 import argparse
 
-def initialize_database(db_path):
-    try:
-        connection = sqlite3.connect(db_path)
-        cursor = connection.cursor()
-        cursor.execute('''CREATE TABLE project_time_tracking 
-                        (id INTEGER PRIMARY KEY, start_time TEXT, end_time TEXT, activities TEXT)''')
-        connection.commit()
-        connection.close()
-    except Exception as e:
-        print(f"Could not initialize databse {db_path} ({e}), exiting...")
-        sys.exit(1)
-
-def start_working(project_name):
-    db_path = f'.\{project_name}_time_tracker.db'
-    if not os.path.exists(db_path): 
-        initialize_database(db_path)
-    try:
-        connection = sqlite3.connect(db_path)
-        cursor = connection.cursor()
-        cursor.execute("INSERT INTO project_time_tracking (start_time) VALUES (datetime('now'))")
-        connection.commit()
-        connection.close()
-    except Exception as e:
-        print(f"Could not insert into databse {db_path} ({e}), exiting...")
-        sys.exit(1)
-    print(f"Started working on the project: {project_name}.")
-
-def stop_working(project_name, activities):
-    db_path = f'.\{project_name}_time_tracker.db'
-    try: 
-        connection = sqlite3.connect(db_path)
-        cursor = connection.cursor()
-        cursor.execute('''UPDATE project_time_tracking 
-                        SET end_time = datetime('now'), activities = ? 
-                        WHERE id = (SELECT MAX(id) FROM project_time_tracking)''', (activities,))
-        connection.commit()
-        connection.close()
-    except Exception as e:
-        print(f"Could not update into databse {db_path} ({e}), exiting...")
-        sys.exit(1)
-    print(f"Stopped working on the project: {project_name} and recorded activities.")
-
-def write_project_to_csv(project_name : str) -> None:
-    db_path = f'.\{project_name}_time_tracker.db'
-    try:
-        db = sqlite3.connect(db_path)
-
-        # Query to list all tables
-        query = "SELECT * FROM project_time_tracking;"
-        df = pd.read_sql_query(query, db)
-    except Exception as e:
-        print(f"Could not read from database {db_path} ({e}), exiting...")
-        sys.exit(1)
+class Storage():
+    def __init__(self, project_name):
+        self.project_name = project_name
+        self._db_path = f'.\{project_name}_time_tracker.db'
+        if not os.path.exists(self._db_path): 
+            self.initialize_database()
     
-    # Convert string timestamps to datetime objects
-    df['start_time'] = pd.to_datetime(df['start_time'])
-    df['end_time'] = pd.to_datetime(df['end_time'])
+    def initialize_database(self):
+        try:
+            connection = sqlite3.connect(self._db_path)
+            cursor = connection.cursor()
+            cursor.execute('''CREATE TABLE project_time_tracking 
+                            (id INTEGER PRIMARY KEY, start_time TEXT, end_time TEXT, activities TEXT)''')
+            connection.commit()
+            connection.close()
+        except Exception as e:
+            print(f"Could not initialize databse {self._db_path} ({e}), exiting...")
+            sys.exit(1)
 
-    df['duration'] = df['end_time'] - df['start_time']
-    print(df)
+    def start_working(self):
+        try:
+            connection = sqlite3.connect(self._db_path)
+            cursor = connection.cursor()
+            cursor.execute("INSERT INTO project_time_tracking (start_time) VALUES (datetime('now'))")
+            connection.commit()
+            connection.close()
+        except Exception as e:
+            print(f"Could not insert into databse {self._db_path} ({e}), exiting...")
+            sys.exit(1)
+        print(f"Started working on the project: {self.project_name}.")
+
+    def stop_working(self, activities):
+        try: 
+            connection = sqlite3.connect(self._db_path)
+            cursor = connection.cursor()
+            cursor.execute('''UPDATE project_time_tracking 
+                            SET end_time = datetime('now'), activities = ? 
+                            WHERE id = (SELECT MAX(id) FROM project_time_tracking)''', (activities,))
+            connection.commit()
+            connection.close()
+        except Exception as e:
+            print(f"Could not update into databse {self._db_path} ({e}), exiting...")
+            sys.exit(1)
+        print(f"Stopped working on the project: {self.project_name} and recorded activities.")
+
+    def write_project_to_csv(self) -> None:
+        db_path = f'.\{self.project_name}_time_tracker.db'
+        try:
+            db = sqlite3.connect(db_path)
+
+            # Query to list all tables
+            query = "SELECT * FROM project_time_tracking;"
+            df = pd.read_sql_query(query, db)
+        except Exception as e:
+            print(f"Could not read from database {db_path} ({e}), exiting...")
+            sys.exit(1)
+        
+        # Convert string timestamps to datetime objects
+        df['start_time'] = pd.to_datetime(df['start_time'])
+        df['end_time'] = pd.to_datetime(df['end_time'])
+
+        df['duration'] = df['end_time'] - df['start_time']
+        print(df)
 
 
 if __name__ == "__main__":
@@ -80,11 +83,11 @@ if __name__ == "__main__":
             description = input("What did you do in this session? ")
         else:
             description = " ".join(args.stop)
-        stop_working(args.projectname, description)
+        Storage(args.projectname).stop_working(description)
     else:  
         if args.start:
-            start_working(args.projectname)
+            Storage(args.projectname).start_working()
         elif args.print:
-            write_project_to_csv(args.projectname)
+            Storage(args.projectname).write_project_to_csv()
         else:  
             arg_parser.print_help() # no valid args, print help
