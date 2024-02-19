80560ebdd57878c1db892d338fe6125d7fb7c212
SakuOrdrTab <leinosensaku@gmail.com>
2024-02-19 15:01:19 +0200

    all now in one file

--- proj_ttrack.py
+++ proj_ttrack.py
@@ -2,6 +2,7 @@ import sqlite3
 import os
 import sys
 import pandas as pd
+import argparse
 
 def initialize_database(db_path):
     connection = sqlite3.connect(db_path)
@@ -21,9 +22,8 @@ def start_working(project_name):
     connection.close()
     print(f"Started working on the project: {project_name}.")
 
-def stop_working(project_name):
-    db_path = f'asdk_time_tracker.db'
-    activities = input("What did you do in this session? ")
+def stop_working(project_name, activities):
+    db_path = f'.\{project_name}_time_tracker.db'
     connection = sqlite3.connect(db_path)
     cursor = connection.cursor()
     cursor.execute('''UPDATE project_time_tracking 
@@ -33,8 +33,8 @@ def stop_working(project_name):
     connection.close()
     print(f"Stopped working on the project: {project_name} and recorded activities.")
 
-def write_project_csv(project_name : str) -> None:
-    db_path = f'{proj_name}.db'
+def write_project_to_csv(project_name : str) -> None:
+    db_path = f'.\{project_name}_time_tracker.db'
     db = sqlite3.connect(db_path)
     # Query to list all tables
     query = "SELECT * FROM project_time_tracking;"
@@ -50,9 +50,23 @@ def write_project_csv(project_name : str) -> None:
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        proj_name = input("Please provide a project name: ")
-        start_working("asdk")
+    arg_parser = argparse.ArgumentParser(description="Track project's working hours.")
+    arg_parser.add_argument('projectname', help="The name of the project is needed as the first argument.")
+    arg_parser.add_argument('-start', action='store_true', help='Start a working period')
+    arg_parser.add_argument('-stop', nargs='*', help='Stop a working period and provide a description (optional)')
+    arg_parser.add_argument('-print', action='store_true', help='Write project time usage to .csv')
+
+    args = arg_parser.parse_args()
+
+    if args.start:
+        start_working(args.projectname)
+    elif args.stop:
+        if not args.stop:  # If no description provided
+            description = input("What did you do in this session? ")
+        else:
+            description = " ".join(args.stop)  # Join list elements into a string
+        stop_working(args.projectname, description)
+    elif args.print:
+        write_project_to_csv(args.projectname)
     else:
-        project_name = sys.argv[1]
-        start_working("asdk")
+        arg_parser.print_help()  # Display help if no valid argument is given
