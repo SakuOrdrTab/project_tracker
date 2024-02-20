caa24bee6b4b7c14b34ea23254b1a3f1e85f034e
SakuOrdrTab <leinosensaku@gmail.com>
2024-02-20 19:31:07 +0200

    streamlined code a bit

--- proj_ttrack.py
+++ proj_ttrack.py
@@ -13,68 +13,55 @@ class Storage():
     
     def initialize_database(self):
         try:
-            connection = sqlite3.connect(self._db_path)
-            cursor = connection.cursor()
-            cursor.execute('''CREATE TABLE project_time_tracking 
-                            (id INTEGER PRIMARY KEY, start_time TEXT, end_time TEXT, activities TEXT)''')
-            connection.commit()
-            connection.close()
+            with sqlite3.connect(self._db_path) as db:
+                db.execute('''CREATE TABLE project_time_tracking 
+                                (id INTEGER PRIMARY KEY, start_time TEXT, end_time TEXT, activities TEXT)''')
         except Exception as e:
-            print(f"Could not initialize databse {self._db_path} ({e}), exiting...")
+            print(f"Could not initialize database {self._db_path} ({e}), exiting...")
             sys.exit(1)
 
     def start_working(self):
         try:
-            connection = sqlite3.connect(self._db_path)
-            cursor = connection.cursor()
-            # Check if there is already an ongoing session
-            cursor.execute("SELECT id FROM project_time_tracking WHERE end_time IS NULL")
-            if cursor.fetchone():
-                print("There is already an ongoing session. Please stop the current session before starting a new one.")
-                return
-            
-            # If no ongoing session, start a new one
-            cursor.execute("INSERT INTO project_time_tracking (start_time) VALUES (datetime('now'))")
-            connection.commit()
+            with sqlite3.connect(self._db_path) as db:
+                # Check if there is already an ongoing session
+                if db.execute("SELECT id FROM project_time_tracking WHERE end_time IS NULL").fetchone():
+                    print("There is already an ongoing session. Please stop the current session before starting a new one.")
+                    return
+                
+                # If no ongoing session, start a new one
+                db.execute("INSERT INTO project_time_tracking (start_time) VALUES (datetime('now'))")
         except Exception as e:
             print(f"Could not insert into databse {self._db_path} ({e}), exiting...")
             sys.exit(1)
         print(f"Started working on the project: {self.project_name}.")
 
-
     def stop_working(self, activities):
-        try: 
-            connection = sqlite3.connect(self._db_path)
-            cursor = connection.cursor()
+        try:
+            with sqlite3.connect(self._db_path) as db:
 
-            # Check if there is an ongoing session
-            cursor.execute("SELECT id FROM project_time_tracking WHERE end_time IS NULL")
-            ongoing_session = cursor.fetchone()
-            if ongoing_session is None:
-                print("No ongoing session to stop. Please start a session first.")
-                return
-            
-            # If there is an ongoing session, update it
-            cursor.execute('''UPDATE project_time_tracking 
-                            SET end_time = datetime('now'), activities = ? 
-                            WHERE id = ?''', (activities, ongoing_session[0],))
-            connection.commit()
+                # Check if there is an ongoing session
+                ongoing_session = db.execute("SELECT id FROM project_time_tracking WHERE end_time IS NULL").fetchone()
+                if ongoing_session is None:
+                    print("No ongoing session to stop. Please start a session first.")
+                    return
+                
+                # If there is an ongoing session, update it
+                db.execute('''UPDATE project_time_tracking 
+                                SET end_time = datetime('now'), activities = ? 
+                                WHERE id = ?''', (activities, ongoing_session[0],))
         except Exception as e:
             print(f"Could not update into databse {self._db_path} ({e}), exiting...")
             sys.exit(1)
         print(f"Stopped working on the project: {self.project_name} and recorded activities.")
 
-
     def write_project_to_csv(self) -> None:
-        db_path = f'.\{self.project_name}_time_tracker.db'
         try:
-            db = sqlite3.connect(db_path)
-
-            # Query to list all tables
-            query = "SELECT * FROM project_time_tracking;"
-            df = pd.read_sql_query(query, db)
+            with sqlite3.connect(self._db_path) as db:
+                # Query to list all tables
+                query = "SELECT * FROM project_time_tracking;"
+                df = pd.read_sql_query(query, db)
         except Exception as e:
-            print(f"Could not read from database {db_path} ({e}), exiting...")
+            print(f"Could not read from database {self._db_path} ({e}), exiting...")
             sys.exit(1)
         
         # Convert string timestamps to datetime objects
@@ -82,7 +69,8 @@ class Storage():
         df['end_time'] = pd.to_datetime(df['end_time'])
 
         df['duration'] = df['end_time'] - df['start_time']
-        print(df)
+        if df.shape[0] < 100: # Don't print million rows to screen
+            print(df) 
         df.to_csv(f"{self.project_name}_time_tracker.csv")
 
 
